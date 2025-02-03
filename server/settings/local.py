811433476ba08cee628da80f2eaaba22a4ffe890
ZTCodeCraft <ztcodecraft@gmail.com>
2025-02-03 17:29:57 +0200

    add whitenoise

--- server/settings/local.py
+++ server/settings/local.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-ohu(or9dn&m+is$94xgu1jhdtwo0q4n5xlbccqj33qdc+-3!mm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
